1cd3edfe414fd9004526d6d8e6983c2f1ac8c019
Thomas Mailund <mailund@birc.au.dk>
2022-03-15 10:41:35 +0100

    removed solutions

--- gsa/ba.go
+++ gsa/ba.go
@@ -6,24 +6,7 @@ package gsa
 // a prefix and a suffix of x[:i+1].
 func Borderarray(x string) []int {
 	ba := make([]int, len(x))
-	for i := 1; i < len(x); i++ {
-		b := ba[i-1]
-
-		for {
-			if x[b] == x[i] {
-				ba[i] = b + 1
-				break
-			}
-
-			if b == 0 {
-				ba[i] = 0
-				break
-			}
-
-			b = ba[b-1]
-		}
-	}
-
+	// FIXME
 	return ba
 }
 
@@ -31,12 +14,7 @@ func Borderarray(x string) []int {
 // This is almost the same as the border array, but ba[i] will be the
 // longest proper border of the string x[:i+1] such that x[ba[i]] != x[i].
 func StrictBorderarray(x string) []int {
-	ba := Borderarray(x)
-	for i := 1; i < len(x)-1; i++ {
-		if ba[i] > 0 && x[ba[i]] == x[i+1] {
-			ba[i] = ba[ba[i]-1]
-		}
-	}
-
+	ba := make([]int, len(x))
+	// FIXME
 	return ba
 }
